1c9f042e9e41679dc9c1f55cc7eb01e440740e62
yjdy <yuanjunfy@163.com>
2025-02-13 19:36:36 +0800

    增加dcn, dcnv2和finalmlp

--- model_zoo/__init__.py
+++ model_zoo/__init__.py
@@ -1,3 +1,6 @@
 from .BST.src import BST
 from .DIN.src import DIN
+from .DCN.DCN_torch.src import DCN
+from .DCNv2.src import DCNv2
+from .FinalMLP.src import FinalMLP
 from .SASRec.src import SASRec
